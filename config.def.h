16fce50fab0aea6a54056d91b3e43f7484493a3d
markus-langgeng <markus.langgeng4@gmail.com>
2023-07-01 14:43:20 +0700

    Add fake fullscreen

--- config.def.h
+++ config.def.h
@@ -51,7 +51,7 @@ static const Rule rules[] = {
 static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
