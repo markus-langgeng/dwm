f75c14a95a1f6c456249d662dff352ab7ac42384
markus-langgeng <markus.langgeng4@gmail.com>
2023-07-01 14:19:18 +0700

    Add winicons

--- config.h
+++ config.h
@@ -3,14 +3,16 @@
 /* appearance */
 static unsigned int borderpx        = 1;        /* border pixel of windows */
 static unsigned int snap            = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int showbar                  = 1;        /* 0 means no bar */
 static int topbar                   = 1;        /* 0 means bottom bar */
+#define ICONSIZE 13   /* icon size */
+#define ICONSPACING 5 /* space between icon and title */
 static char font[]                  = "monospace:pixelsize=13:antialias=true:autohint=true";
 static char dmenufont[]             = "monospace:pixelsize=13:antialias=true:autohint=true";
 static const char *fonts[]          = {
