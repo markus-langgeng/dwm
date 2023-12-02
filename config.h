67bc8af29fb638297a416a84626a9232e9e69a74
markus-langgeng <markus.langgeng4@gmail.com>
2023-12-02 17:24:44 +0700

    add swallow

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10:antialias=true:autohint=true" };
@@ -26,13 +27,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class              instance    title                 tags mask     isfloating   monitor */
-	{ "Gimp",             NULL,       NULL,                 1 << 3,       1,           -1 },
-	{ "krita",            NULL,       NULL,                 1 << 3,       1,           -1 },
-	{ "kdenlive",         NULL,       NULL,                 1 << 4,       1,           -1 },
-  { "Mullvad Browser",  NULL,       NULL,                 3 << 1,       0,           -1 },
-	{ "firefox",          NULL,       NULL,                 1 << 2,       0,           -1 },
-	{ NULL,               NULL,       "Event Tester",       0,            1,           -1 }, /* xev */
+	/* class              instance    title                 tags mask     isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",             NULL,       NULL,                 1 << 3,       0,            0,           1,        -1 },
+	{ "krita",            NULL,       NULL,                 1 << 3,       0,            0,           1,        -1 },
+	{ "kdenlive",         NULL,       NULL,                 1 << 4,       0,            0,           1,        -1 },
+  { "Mullvad Browser",  NULL,       NULL,                 3 << 1,       0,            0,           0,        -1 },
+	{ "firefox",          NULL,       NULL,                 1 << 2,       0,            0,           0,        -1 },
+	{ NULL,               NULL,       "Event Tester",       0,            0,            0,           1,        -1 }, /* xev */
+	{ "St",               NULL,       NULL,                 0,            0,            1,           0,        -1 },
 };
 
 /* layout(s) */
